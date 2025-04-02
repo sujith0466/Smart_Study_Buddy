0a6a51719754eb7c4f35d1bac0903e5186268b2b
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-04-02 22:04:58 +0530

    Delete Study_Buddy directory

--- Study_Buddy/recommender.py
+++
@@ -1,6 +0,0 @@
-def generate_study_plan(subjects, past_scores, study_method):
-    study_plan = []
-    for subject, score in zip(subjects, past_scores):
-        recommended_hours = max(1, (100 - score) / 10)  # Example formula
-        study_plan.append({'subject': subject, 'hours': recommended_hours})
-    return study_plan
