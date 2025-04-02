70c783f4651bcaf97a881760bd8249aa8408a0ad
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-04-02 22:08:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
-from Study_Buddy.database import init_db  # Adjusted import path
+from database import init_db  # Adjusted import path
 from recommender import generate_study_plan
 from scheduler import create_schedule
 from youtube_api import get_youtube_suggestions
