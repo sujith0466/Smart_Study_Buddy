e1c0ae22fb1d2bf1fcdb1b35ad2f8a9251382c3b
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-04-03 18:34:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,4 +5,7 @@ db = SQLAlchemy()
 def init_db(app):
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        try:
+            db.create_all()  # Create database tables
+        except Exception as e:
+            print(f"Error creating database tables: {e}")
