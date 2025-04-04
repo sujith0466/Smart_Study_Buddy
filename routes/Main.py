1c5a7d45696df90c23841d9c3f013d07d53e93e7
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-04-04 14:59:37 +0530

    Update and rename api.py to Main.py

---
+++ routes/Main.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template, request
+from recommender import generate_study_plan
+from scheduler import create_schedule
+from youtube_api import get_youtube_suggestions
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/generate', methods=['POST'])
+def generate():
+    subjects = request.form.getlist('subjects[]')
+    scores = request.form.getlist('scores[]')
+    study_method = request.form.get('study_method')
+
+    if not subjects or not scores or not study_method:
+        return render_template('index.html', error="All fields are required.")
+
+    try:
+        scores = list(map(int, scores))
+    except ValueError:
+        return render_template('index.html', error="Scores must be numbers.")
+
+    recommendations = generate_study_plan(subjects, scores, study_method)
+    schedule = create_schedule(recommendations)
+    resources = get_youtube_suggestions(subjects)
+
+    return render_template('result.html', schedule=schedule, resources=resources)
